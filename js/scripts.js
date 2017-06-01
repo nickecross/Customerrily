aadf80e2f0e2979c5927c29b8a39a1913f216c50
Nick Cross <nick@customerrily.com>
2017-06-01 09:12:43 -0400

    Design and content updates.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+    //Scripts related to GForm submit
+    // var submitted=false;
+    // $('#gform').on('submit', function(e) {
+    // $('#gform *').fadeOut(2000);
+    // $('#gform').prepend('<p style="text-align: center;">Thank you for your submission! We will be in touch shortly.</p>');
+    // });
+    var submitted=false;
+    $('#gform').on('submit', function(e) {
+    $('#gform *').fadeOut(2000);
+    $('#gform').prepend('<p style="text-align: center;">Thank you for your submission! We will be in touch shortly.</p>');
+    });
 });
