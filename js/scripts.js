f7a81afea7a684ab77cab6701f2f50a8abcae617
Nick Cross <nick@customerrily.com>
2017-06-22 18:28:22 -0400

    Troubleshooting menu link scrolling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    console.log("scripts are loading correctly!");
     $('.modal').modal();
+    $("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+  });
     //Scripts related to GForm submit
     // var submitted=false;
     // $('#gform').on('submit', function(e) {
     // $('#gform *').fadeOut(2000);
     // $('#gform').prepend('<p style="text-align: center;">Thank you for your submission! We will be in touch shortly.</p>');
     // });
-    var submitted=false;
-    $('#gform').on('submit', function(e) {
-    $('#gform *').fadeOut(2000);
-    $('#gform').prepend('<p style="text-align: center;">Thank you for your submission! We will be in touch shortly.</p>');
-    });
-    $('.scrollspy').scrollSpy({
-      scrollOffset:100
-    });
+    // var submitted=false;
+    // $('#gform').on('submit', function(e) {
+    // $('#gform *').fadeOut(2000);
+    // $('#gform').prepend('<p style="text-align: center;">Thank you for your submission! We will be in touch shortly.</p>');
+    // });
 });
